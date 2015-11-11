1e9adfbacc51e3c085b69b9df0add79ffae4535b
kelvinzero <joshuacotes@gmail.com>
2015-11-11 09:34:04 -0800

    Create Node.java

---
+++ dynamicprog/matrixtools/node/Node.java
@@ -0,0 +1,14 @@
+/*
+* A generic node that holds an int
+*/
+public class Node {
+
+  public int weight;
+  
+  public Node() {
+  }
+  
+  public Node(int weight) {
+    this.weight = weight;
+  }
+}
