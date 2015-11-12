62784240a12bf9130a7f439ce67ce549d6a87c1a
kelvinzero <joshuacotes@gmail.com>
2015-11-11 20:03:13 -0800

    Update DynamicCollection.java

--- dynamicprog/matrixtools/DynamicCollection.java
+++ dynamicprog/matrixtools/DynamicCollection.java
@@ -97,6 +97,31 @@ public static int shortestPath(Node[][] W, int j, int k, int M, int N) {
         }
     }
     
+     /* queens algorithm modified to stop searching after the first solution is found */ 
+    public static boolean queens_firstonly(int[] C, int queen) { // queen i goes in queen i4
+
+        // first make sure this queen isn't in conflict with previously placed queens,
+        // method call backs out if this queen doesn't fit here
+        if (safespace(C, queen)) { // - - - n
+            // if a safespace has been found in the last row a solution set is complete,
+            // print the chess board
+            if (queen == C.length - 1) { // - - - c1
+                System.out.println(); // - - - c2
+                printBoard(C); // - - -  c3
+                return false;
+            }
+            // if this safespace is not in last row, try to add a queen in the next row
+            else {  // - - - - - c4
+                for (int column = 0; column < C.length; column++) { // - - - - c5
+                    C[queen + 1] = column;  // - - - - c6
+                    if(!queens_firstonly(C, queen + 1)) // - - - - n!
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    
     /**
      * checks columns 0 to q-1 that they are not in conflict with the queen
      * in the current row.
